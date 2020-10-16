4fa304ccabbdee0fa01c628d5745945809393d26
DrawMaGan <mustache301@gmail.com>
2020-10-15 21:02:52 -0300

    NLW3-V2
    versao 2 do site

---
+++ public/src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.2145525",
+        lng: "-49.6440873",
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1600711725615-d7dfb2215244?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1601564267677-a36d03ec2be5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "1"
+    },
+    {   
+        id: 2,
+        lat: "-27.2145525",
+        lng: "-49.6540873",
+        name: "Lar dos Meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1600711725615-d7dfb2215244?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1601564267677-a36d03ec2be5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "0"
+
+
+    }
+        
+
+
+]
